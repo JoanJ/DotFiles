5cb3bd8596b8252031922b915ec873682070a857
Joan Juventench <joan.juventench@launchmetrics.com>
2018-10-09 12:27:27 +0200

    fix script

--- lib/linux.sh
+++ lib/linux.sh
@@ -122,7 +122,7 @@ function docker() {
   sudo usermod -aG docker $USER
 
   # Install docker-compose
-  sudo curl -sL https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo curl -sL https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 
 }
